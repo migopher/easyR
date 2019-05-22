4ba42b5773111cded885b4c5813b334e28aface4
ixuzl <863582605@qq.com>
2019-05-22 10:26:20 +0800

    更新文件

--- ch-9.R
+++ ch-9.R
@@ -2,10 +2,10 @@ lang <- c("C","PHP","JAVA","GO","R")
 name <- c("lisi","zhangsna","zhangsan","wangwu","wusong")
 num <- c(1,2,6,3,4)
 myData <- data.frame(num,name,lang)
-print(myData)
-attach(myData)
-  summary(name)
-  plot(num)
-  plot(lang)
-detach(myData)
-print(myData)
+# print(myData)
+
+with(myData,{
+summary(num,name，lang)
+
+}
+)
